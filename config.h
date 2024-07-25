f2a49dcc638d19c9310cfe142cf461f4dc055c86
Aria_RZA <ariarza1225@gmail.com>
2024-07-25 01:20:45 -0500

    Add project files.

---
+++ config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// config 
+
+constexpr auto WIDTH = 1200; // window width
+constexpr auto HEIGHT = 1000; // window height
+
+constexpr auto LEN = 20; // grid length
+constexpr auto XLEN = 28; // grid diagonal
+
