e4c04a9ff27b3c2da8522d9f5064e26b9faca209
Aria_RZA <ariarza1225@gmail.com>
2024-07-28 20:35:54 -0500

    implement main.h

--- main.h
+++ main.h
@@ -1,2 +1,17 @@
 #pragma once
 #include "strategy.h"
+
+namespace NAVIGATION
+{
+	class Navigate
+	{
+	public:
+		Navigate();
+		bool process(const int& _dist_type, const int* algo_type);
+
+	public:
+		vector<Node*> result_nodes;
+		unique_ptr<GridMapBase> grid_map_ptr;
+		unique_ptr<StrategyBase> strategy_ptr;
+	};
+}
