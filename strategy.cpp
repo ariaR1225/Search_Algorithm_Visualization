95d5e55439f026a72769778a17026a7ce7982e4e
Aria_RZA <ariarza1225@gmail.com>
2024-07-28 20:32:00 -0500

    update strategy.cpp

--- strategy.cpp
+++ strategy.cpp
@@ -15,7 +15,111 @@ namespace NAVIGATION
 
 	bool AStar::search(const unique_ptr<GridMapBase>& map, vector<Node*>& result_nodes)
 	{
+		// init start
+		open_queue.emplace(*map->start_node);
+		open_set.insert(map->start_node);
+		Node* current_node = map->start_node; // ptr to start
 
+		// set move direction 
+		vector<pair<int, int>> move_step = // use pair for direction
+		{
+			pair <int, int>(0, 1),
+			pair <int, int>(0, -1),
+			pair <int, int>(1, 0),
+			pair <int, int>(-1, 0),
+			pair <int, int>(-1, 1),
+			pair <int, int>(-1, -1),
+			pair <int, int>(1, -1),
+			pair <int, int>(1, 1),
+		};
+
+		// search
+		while (!open_queue.empty())
+		{
+			// current node
+			int r = open_queue.top().row;
+			int c = open_queue.top().col;
+			current_node = &map->grid_map[r][c];
+			cout << "CURRENT NODE: f = " << current_node->f << ", g = " << current_node->g << ", h = " << current_node->h << endl;
+
+			// draw close
+			setfillcolor(YELLOW);
+			drawNodeStep(map->start_node, map->end_node, current_node, SEARCH_DELAY);
+
+			open_queue.pop();
+			open_set.erase(current_node);
+			close_set.insert(current_node);
+
+			// reach end node
+			if (current_node == map->end_node)
+			{
+				path_len = current_node->g;
+
+				// store to result_nodes from current to start
+				while (current_node->parent_node) // start has empty parent_node
+				{
+					current_node->node_type = NodeType::way_pt;
+					result_nodes.emplace_back(current_node);
+					current_node = current_node->parent_node;
+				}
+				reverse(result_nodes.begin(), result_nodes.end());
+				cout << "Successful!" << endl;
+
+				// recover start and end node type
+				map->start_node->node_type = NodeType::start;
+				map->end_node->node_type = NodeType::end;
+
+				// print
+				printResult(result_nodes);
+				map->printMap();
+				drawResult(map->start_node, map->end_node, result_nodes);
+				return true;
+			}
+
+			// search
+			for (auto i = move_step.begin(); i != move_step.end(); i++)
+			{
+				Node temp_node(r + i->first, c + i->second);
+				if (map->outOfMap(temp_node)) // skip if out of bound
+				{
+					continue;
+				}
+
+				Node* next_node = &map->grid_map[temp_node.row][temp_node.col]; // create next node if node out of bound
+				if (close_set.find(next_node) != close_set.end() // if is in close_set (already visited)
+					|| map->obs_nodes.find(next_node) != map->obs_nodes.end()) // or in obs
+				{
+					continue; // skip
+				}
+
+				step_len = abs(i->first) == abs(i->second) ? XLEN : LEN; // either side or diagonal
+				if (open_set.find(next_node) != open_set.end()) // already in open
+				{
+					if (current_node->g + step_len < next_node->g)
+					{
+						calcCost(*current_node, *next_node, *map->end_node);
+						next_node->parent_node = current_node;
+					}
+				}
+				else // not in open, add to open
+				{
+					calcCost(*current_node, *next_node, *map->end_node);
+					next_node->parent_node = current_node;
+					open_queue.emplace(*next_node);
+					open_set.insert(next_node);
+
+					// draw open
+					setfillcolor(LIGHTBLUE);
+					drawNodeStep(map->start_node, map->end_node, current_node, SEARCH_DELAY);
+				}
+			}
+
+		}
+
+		// fail to search
+		cout << "Failed!" << endl;
+		drawResult(map->start_node, map->end_node, result_nodes);
+		return false;
 	}
 
 	int AStar::heuristicDist(const Node& node1, const Node& node2)
@@ -54,7 +158,7 @@ namespace NAVIGATION
 			next_node.g = 0;
 		}
 		next_node.f = next_node.g + next_node.h;
-		cout << "f = " << next_node.f << ", g = " << next_node.g << ", h = " << next_node.h << endl;
+		cout << "COST: f = " << next_node.f << ", g = " << next_node.g << ", h = " << next_node.h << endl;
 	}
 
 	void AStar::printResult(const vector<Node*>& result_nodes)
